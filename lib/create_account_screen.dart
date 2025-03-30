2b88e92757a9eedf9369ecae3a5259c109c20b8d
TerkumaAmos <agberterkuma2020@gmail.com>
2025-03-30 03:46:13 +0100

    new

---
+++ lib/create_account_screen.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class CreateAccountScreen extends StatelessWidget {
+  const CreateAccountScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: RadialGradient(
+            center: Alignment.center,
+            radius: 1.5,
+            colors: [
+              Colors.white,
+              Color(0xFFfbf1af), // Light yellow
+              Color(0xFFf9e07f),
+            ],
+            stops: [0.2, 0.7, 1.0],
+          ),
+        ),
+        child: Stack(
+          children: [
+            Positioned(
+              top: 42,
+              left: 53,
+              child: Container(
+                width: 12,
+                height: 12,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color(
+                    0xFFFEDC00,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 72,
+              left: 330,
+              child: Container(
+                width: 30,
+                height: 30,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color(
+                    0xFFFEDC00,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 412,
+              left: -18,
+              child: Container(
+                width: 28,
+                height: 28,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color(
+                    0xFFFEDC00,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 50,
+              left: 9,
+              child: SvgPicture.asset(
+                "assets/Group 2.svg",
+                width: 94.5,
+                height: 40.62,
+              ),
+            ),
+            Positioned(
+              top: 484,
+              left: 367,
+              child: SvgPicture.asset(
+                "assets/Group 1.svg",
+                width: 94.5,
+                height: 40.62,
+              ),
+            ),
+            Positioned(
+              top: 613,
+              left: 331,
+              child: Container(
+                width: 22,
+                height: 22,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color(
+                    0xFFFEDC00,
+                  ),
+                ),
+              ),
+            ),
+            Column(
+              children: [
+                const SizedBox(height: 130),
+                const Center(
+                  child: Text(
+                    "Create an account",
+                    style: TextStyle(fontFamily: "WideTrial", fontSize: 24),
+                  ),
+                ),
+                const SizedBox(height: 370),
+                Container(
+                  height: 50,
+                  width: 300,
+                  child: ElevatedButton.icon(
+                    style: const ButtonStyle(
+                      backgroundColor: WidgetStatePropertyAll(
+                        Color(0xFFFEDC00),
+                      ),
+                    ),
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.mail,
+                      color: Colors.white,
+                    ),
+                    label: const Text(
+                      "Continue with Email",
+                      style: TextStyle(
+                          color: Colors.white, fontFamily: "WideTrial"),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  width: 300,
+                  height: 50,
+                  child: ElevatedButton(
+                    style: const ButtonStyle(
+                      side: WidgetStatePropertyAll(
+                        BorderSide(color: Colors.black),
+                      ),
+                      backgroundColor:
+                          WidgetStatePropertyAll(Colors.transparent),
+                    ),
+                    onPressed: () {},
+                    child: Row(
+                      children: [
+                        SvgPicture.asset("assets/Vector1.svg"),
+                        const SizedBox(width: 30),
+                        const Text(
+                          "Continue with facebook",
+                          style: TextStyle(
+                              color: Colors.black, fontFamily: "WideTrial"),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,3 +1,4 @@
+import 'package:dating_app/create_account_screen.dart';
 import 'package:dating_app/login_screen.dart';
 import 'package:dating_app/onboard_screen1.dart';
 import 'package:dating_app/onboard_screen2.dart';
@@ -25,7 +26,7 @@ class _SplashScreenState extends State<SplashScreen> {
         Navigator.push(
           context,
           MaterialPageRoute(
-            builder: (context) => LoginScreen(),
+            builder: (context) => CreateAccountScreen(),
           ),
         );
       },
