61226132c8d071fda089a96f6588a6027f764131
TerkumaAmos <agberterkuma2020@gmail.com>
2025-04-03 03:11:39 +0100

    new

--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -17,218 +17,220 @@ class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: RadialGradient(
-            center: Alignment.center,
-            radius: 1.5,
-            colors: [
-              Colors.white,
-              Color(0xFFfbf1af), // Light yellow
-              Color(0xFFf9e07f),
-            ],
-            stops: [0.2, 0.7, 1.0],
+      body: SingleChildScrollView(
+        child: Container(
+          decoration: const BoxDecoration(
+            gradient: RadialGradient(
+              center: Alignment.center,
+              radius: 1.5,
+              colors: [
+                Colors.white,
+                Color(0xFFfbf1af), // Light yellow
+                Color(0xFFf9e07f),
+              ],
+              stops: [0.2, 0.7, 1.0],
+            ),
           ),
-        ),
-        child: Stack(
-          children: [
-            Positioned(
-              top: 72,
-              left: 330,
-              child: Container(
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Color(
-                    0xFFFEDC00,
+          child: Stack(
+            children: [
+              Positioned(
+                top: 72,
+                left: 330,
+                child: Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color(
+                      0xFFFEDC00,
+                    ),
                   ),
-                ),
-                width: 30,
-                height: 30,
-              ),
-            ),
-            Positioned(
-              top: 42,
-              left: 53,
-              child: Container(
-                width: 12,
-                height: 12,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Color(
-                    0xFFFEDC00,
-                  ),
+                  width: 30,
+                  height: 30,
                 ),
               ),
-            ),
-            Positioned(
-              top: 26.4,
-              left: 20,
-              child: SvgPicture.asset(
-                "assets/Group 2.svg",
-              ),
-            ),
-            Positioned(
-              top: 354,
-              left: 79,
-              child: Container(
-                width: 28,
-                height: 28,
-                decoration: BoxDecoration(
-                  color: Color(
-                    0xFFFEDC00,
-                  ),
-                  shape: BoxShape.circle,
-                ),
-              ),
-            ),
-            Positioned(
-              top: 649,
-              left: 342,
-              child: Container(
-                width: 22,
-                height: 22,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Color(
-                    0xFFFEDC00,
+              Positioned(
+                top: 42,
+                left: 53,
+                child: Container(
+                  width: 12,
+                  height: 12,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color(
+                      0xFFFEDC00,
+                    ),
                   ),
                 ),
               ),
-            ),
-            Positioned(
-              top: 521,
-              left: 367,
-              child: SvgPicture.asset(
-                "assets/Group 1.svg",
+              Positioned(
+                top: 26.4,
+                left: 20,
+                child: SvgPicture.asset(
+                  "assets/Group 2.svg",
+                ),
               ),
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                SizedBox(height: 135),
-                Center(
-                  child: Container(
+              Positioned(
+                top: 354,
+                left: 79,
+                child: Container(
+                  width: 28,
+                  height: 28,
+                  decoration: BoxDecoration(
+                    color: Color(
+                      0xFFFEDC00,
+                    ),
+                    shape: BoxShape.circle,
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 649,
+                left: 342,
+                child: Container(
+                  width: 22,
+                  height: 22,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color(
+                      0xFFFEDC00,
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 521,
+                left: 367,
+                child: SvgPicture.asset(
+                  "assets/Group 1.svg",
+                ),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(height: 135),
+                  Center(
+                    child: Container(
+                      alignment: Alignment.centerLeft,
+                      padding: EdgeInsets.only(left: 29),
+                      child: Text(
+                        "Sign in",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: "WideTrial",
+                          fontSize: 24,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
                     alignment: Alignment.centerLeft,
                     padding: EdgeInsets.only(left: 29),
                     child: Text(
-                      "Sign in",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontFamily: "WideTrial",
-                        fontSize: 24,
+                      "Please enter below the details to continue",
+                      style: TextStyle(fontFamily: "WideTrial", fontSize: 12),
+                    ),
+                  ),
+                  SizedBox(height: 56),
+                  Container(
+                    height: 60,
+                    width: 360,
+                    decoration: BoxDecoration(),
+                    child: AppTextField(
+                      prefixIcon: Icon(Icons.mail),
+                      hintText: "Email",
+                      hintStyle: TextStyle(fontFamily: "WideTrial"),
+                      controller: emailController,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Container(
+                    height: 60,
+                    width: 360,
+                    child: Password(
+                      passwordController: passwordController,
+                    ),
+                  ),
+                  SizedBox(height: 2),
+                  Row(
+                    //  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.only(left: 30),
+                        child: SvgPicture.asset(
+                          "assets/Rectangle 32.svg",
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        "Remember me",
+                        style: TextStyle(fontFamily: "WideTrial"),
+                      ),
+                      SizedBox(width: 100),
+                      Text(
+                        "Forgot password?",
+                        style: TextStyle(fontFamily: "WideTrial", fontSize: 10),
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 160),
+                  Container(
+                    height: 56,
+                    width: 360,
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStatePropertyAll(
+                          Color(0xFFFEDC00),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        "Sign in",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: "WideTrial",
+                        ),
                       ),
                     ),
                   ),
-                ),
-                Container(
-                  alignment: Alignment.centerLeft,
-                  padding: EdgeInsets.only(left: 29),
-                  child: Text(
-                    "Please enter below the details to continue",
-                    style: TextStyle(fontFamily: "WideTrial", fontSize: 12),
-                  ),
-                ),
-                SizedBox(height: 56),
-                Container(
-                  height: 60,
-                  width: 360,
-                  decoration: BoxDecoration(),
-                  child: AppTextField(
-                    prefixIcon: Icon(Icons.mail),
-                    hintText: "Email",
-                    hintStyle: TextStyle(fontFamily: "WideTrial"),
-                    controller: emailController,
-                  ),
-                ),
-                SizedBox(height: 20),
-                Container(
-                  height: 60,
-                  width: 360,
-                  child: Password(
-                    passwordController: passwordController,
-                  ),
-                ),
-                SizedBox(height: 2),
-                Row(
-                  //  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      padding: EdgeInsets.only(left: 30),
-                      child: SvgPicture.asset(
-                        "assets/Rectangle 32.svg",
+                  SizedBox(height: 30),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      SvgPicture.asset(
+                        'assets/Vectors 3.svg',
                       ),
-                    ),
-                    SizedBox(width: 10),
-                    Text(
-                      "Remember me",
-                      style: TextStyle(fontFamily: "WideTrial"),
-                    ),
-                    SizedBox(width: 100),
-                    Text(
-                      "Forgot password?",
-                      style: TextStyle(fontFamily: "WideTrial", fontSize: 10),
-                    )
-                  ],
-                ),
-                SizedBox(height: 160),
-                Container(
-                  height: 56,
-                  width: 360,
-                  child: ElevatedButton(
-                    style: ButtonStyle(
-                      backgroundColor: WidgetStatePropertyAll(
-                        Color(0xFFFEDC00),
+                      SizedBox(width: 10),
+                      Text(
+                        "or continue with",
+                        style: TextStyle(fontFamily: 'WideTrial', fontSize: 10),
                       ),
-                    ),
-                    onPressed: () {},
-                    child: Text(
-                      "Sign in",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontFamily: "WideTrial",
+                      SvgPicture.asset(
+                        'assets/Vectors 3.svg',
                       ),
-                    ),
+                    ],
                   ),
-                ),
-                SizedBox(height: 30),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    SvgPicture.asset(
-                      'assets/Vectors 3.svg',
-                    ),
-                    SizedBox(width: 10),
-                    Text(
-                      "or continue with",
-                      style: TextStyle(fontFamily: 'WideTrial', fontSize: 10),
-                    ),
-                    SvgPicture.asset(
-                      'assets/Vectors 3.svg',
-                    ),
-                  ],
-                ),
-                SizedBox(height: 30),
-                SvgPicture.asset('assets/Group 10.svg'),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    Text(
-                      "Don't have an account?",
-                      style: TextStyle(fontFamily: 'WideTrial', fontSize: 10),
-                    ),
-                    Text(
-                      "Sign up",
-                      style: TextStyle(
-                          fontFamily: 'WideTrial',
-                          fontSize: 10,
-                          color: Color(0xFFFEDC00)),
-                    ),
-                  ],
-                )
-              ],
-            ),
-          ],
+                  SizedBox(height: 30),
+                  SvgPicture.asset('assets/Group 10.svg'),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text(
+                        "Don't have an account?",
+                        style: TextStyle(fontFamily: 'WideTrial', fontSize: 10),
+                      ),
+                      Text(
+                        "Sign up",
+                        style: TextStyle(
+                            fontFamily: 'WideTrial',
+                            fontSize: 10,
+                            color: Color(0xFFFEDC00)),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
